e9adeefa8ed35d038c846687ff19393570f7dd73
Alejandro Torres <aleth2k@gmail.com>
2024-04-13 19:11:26 -0600

    falta poco

--- vite/index.js
+++ vite/index.js
@@ -1,32 +1,30 @@
 import Vara from "vara";
 
-let windowHeight = window.innerHeight;
-
 let vara = new Vara(
-    "#container",
-    "./fonts/font.json",
-    [
-      {
-        text: "Lisset Mis XV años",
-        textAlign: "center",
-        strokeWidth: 2.5,
-        color: "#e2e7ec",
-        y: 35,
-        duration: 7500,
-        id: "sphinx",
-      },
-    ],
+  "#container",
+  "./fonts/font.json",
+  [
     {
-      fontSize: 52,
-    }
-  );
+      text: "Lisset Mis XV años",
+      textAlign: "center",
+      strokeWidth: 2.5,
+      color: "#ffffff",
+      y: 35,
+      duration: 7500,
+      id: "sphinx",
+    },
+  ],
+  {
+    fontSize: 52,
+  }
+);
 
+vara.ready(function () {
+  vara.draw("sphinx");
+  document.getElementsByTagName("g")[0].style.filter =
+    "drop-shadow(3px 5px 2px  rgba(0, 188, 128, 15))";
+});
 
-  vara.ready(function () {
-    vara.draw("sphinx");
-    document.getElementsByTagName("g")[0].style.filter = "drop-shadow(3px 5px 2px  rgba(0, 188, 128, 10))";
-  });
-  window.onresize = function() {
-    // Recargar la página al redimensionar
-    location.reload();
-  };
+window.onresize = function () {
+  location.reload();
+};
